c4fbdd52e75f88472bb41d7393360f0b13367bf7
hadi dehdari <hadi.dehdari@gmail.com>
2025-05-02 16:34:29 +0330

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        },
+    },
+    resolve: {
+        alias: {
+            '$': 'jQuery',
+        },
+    },
+});
